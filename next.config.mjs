bd987b11fc1528ce5a250fd4125d2552cdbb7d99
Adarsh-S18 <adarshspillai2001@gmail.com>
2025-08-28 22:24:21 +0530

    Update the config file for handling access control allow origins

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,PATCH,DELETE,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Authorization, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+        ],
+      },
+    ];
+  },
 };
 export default nextConfig;
